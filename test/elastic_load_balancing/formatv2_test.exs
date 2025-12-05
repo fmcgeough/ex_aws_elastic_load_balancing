5ed9fe813d72dadc4baa5f43c2c13cf912ce0265
Frank McGeough <fmcgeough@gmail.com>
2025-12-05 17:15:43 -0500

    Adding more unit tests

--- test/elastic_load_balancing/formatv2_test.exs
+++ test/elastic_load_balancing/formatv2_test.exs
@@ -2,30 +2,63 @@ defmodule ExAws.ElasticLoadBalancing.FormatV2Test do
   use ExUnit.Case
 
   alias ExAws.ElasticLoadBalancing.FormatV2
+  alias ExAws.Utils
 
-  describe "add_trust_store_revocations_opts" do
-    setup do
-      expected = [
-        {"TrustStoreRevocations.RevocationContents.1.RevocationType", "CRL"},
-        {"TrustStoreRevocations.RevocationContents.1.S3Bucket", "test_bucket"},
-        {"TrustStoreRevocations.RevocationContents.2.RevocationType", "CRL"},
-        {"TrustStoreRevocations.RevocationContents.2.S3Bucket", "test_bucket2"}
-      ]
+  test "add_trust_store_revocations_opts" do
+    revocation1 = %{revocation_type: "CRL", s3_bucket: "test_bucket"}
+    revocation2 = %{revocation_type: "CRL", s3_bucket: "test_bucket2"}
 
-      revocation1 = %{revocation_type: "CRL", s3_bucket: "test_bucket"}
-      revocation2 = %{revocation_type: "CRL", s3_bucket: "test_bucket2"}
+    result = build_result(revocation_contents: [revocation1, revocation2])
 
-      {:ok, expected: expected, revocation_contents: [revocation1, revocation2]}
-    end
+    assert %{
+             "RevocationContents.member.1.RevocationType" => "CRL",
+             "RevocationContents.member.1.S3Bucket" => "test_bucket",
+             "RevocationContents.member.2.RevocationType" => "CRL",
+             "RevocationContents.member.2.S3Bucket" => "test_bucket2"
+           } == result
+  end
 
-    test "as key list", context do
-      opts = [{:revocation_contents, context.revocation_contents}]
-      assert context.expected == FormatV2.format_param({:trust_store_revocations, opts})
-    end
+  test "create_listener_opts" do
+    result =
+      build_result(
+        alpn_policy: ["HTTP1Only", "HTTP2Only"],
+        certificates: [%{certificate_arn: "certificate_arn", is_default: true}],
+        mutual_authentication: %{
+          advertise_trust_store_ca_names: "on",
+          ignore_client_certificate_expiry: true,
+          mode: "verify",
+          trust_store_arn: "trust_store_arn",
+          trust_store_association_status: "active"
+        },
+        port: 65_535,
+        protocol: "HTTP",
+        ssl_policy: "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09",
+        tags: [%{key: "key1", value: "value1"}, %{key: "key2", value: "value2"}]
+      )
 
-    test "as map", context do
-      opts = %{revocation_contents: context.revocation_contents}
-      assert context.expected == FormatV2.format_param({:trust_store_revocations, opts})
-    end
+    assert %{
+             "AlpnPolicy.member.1" => "HTTP1Only",
+             "AlpnPolicy.member.2" => "HTTP2Only",
+             "Certificates.member.1.CertificateArn" => "certificate_arn",
+             "Certificates.member.1.IsDefault" => true,
+             "MutualAuthentication.AdvertiseTrustStoreCaNames" => "on",
+             "MutualAuthentication.IgnoreClientCertificateExpiry" => true,
+             "MutualAuthentication.Mode" => "verify",
+             "MutualAuthentication.TrustStoreArn" => "trust_store_arn",
+             "MutualAuthentication.TrustStoreAssociationStatus" => "active",
+             "Port" => 65_535,
+             "Protocol" => "HTTP",
+             "SslPolicy" => "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09",
+             "Tags.member.1.Key" => "key1",
+             "Tags.member.1.Value" => "value1",
+             "Tags.member.2.Key" => "key2",
+             "Tags.member.2.Value" => "value2"
+           } == result
+  end
+
+  defp build_result(opts) do
+    opts
+    |> Enum.flat_map(&FormatV2.format_param/1)
+    |> Utils.filter_nil_params()
   end
 end
